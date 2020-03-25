d87581ec244ce7df2bf8c5fae42badbcba225520
Blue Lindsey <linzblue5@gmail.com>
2020-03-25 11:59:00 -0500

    Finished up getting the cards to flip on click.

--- src/components/MemoryCard.jsx
+++ src/components/MemoryCard.jsx
@@ -2,10 +2,25 @@ import React, { Component } from "react";
 import './MemoryCard.css';
 
 class MemoryCard extends Component {
+    state = {
+        isFlipped: false
+      };
+    
+      clickHandler = () => {
+        this.setState({
+          isFlipped: !this.state.isFlipped
+        });
+      };
+    
+    
     render() {
+        const { isFlipped } = this.state;
+
+        const memoryCardInnerClass =
+          'MemoryCardInner ' + (isFlipped === true && 'flipped');
         return (
             <div className="MemoryCard" onClick={this.clickHandler}>
-                <div className="MemoryCardInner">
+                <div className={memoryCardInnerClass}>
                     <div className="MemoryCardBack">
                         <img src="https://www.digitalcrafts.com/img/digitalcrafts-logo-white-y.png" alt="DcLogo" /></div>
                     <div className="MemoryCardFront">
@@ -14,10 +29,6 @@ class MemoryCard extends Component {
                 </div>
             </div>
         )
-    }
-
-    clickHandler() {
-        alert("card clicked");
     }
 }
 
